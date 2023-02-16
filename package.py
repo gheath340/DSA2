08c6b44ea71239f9988de9e2c6d67cac6056fd39
gheath340 <heathsmanil0@gmail.com>
2023-02-16 11:15:03 -0700

    package class

---
+++ package.py
@@ -0,0 +1,59 @@
+
+class Package:
+    def __init__(self, id, address, city, state, zip, deadline, mass):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self. deadline = deadline
+        self.mass = mass
+        self.status = "at hub"
+
+    def getID(self):
+        return self.id
+
+    def setID(self, value):
+        self.id = value
+
+    def getAddress(self):
+        return self.address
+
+    def setAddress(self, value):
+        self.addressd = value
+        
+    def getCity(self):
+        return self.city
+
+    def setCity(self, value):
+        self.city = value
+    
+    def getState(self):
+        return self.state
+
+    def setState(self, value):
+        self.state = value
+
+    def getZip(self):
+        return self.zip
+
+    def setZip(self, value):
+        self.zip = value
+
+    def getDeadline(self):
+        return self.deadline
+
+    def setDeadline(self, value):
+        self.deadline = value
+
+    def getMass(self):
+        return self.mass
+
+    def setMass(self, value):
+        self.mass = value
+
+    def getStatus(self):
+        return self.status
+
+    def setStatus(self, value):
+        self.status = value
---
+++ truck.py
@@ -0,0 +1,5 @@
+
+
+class Truck:
+    def __init__(self, ):
+        
