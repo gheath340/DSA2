aaa255aa4fd3546c49d5c6e575ece166c8acc6b1
gheath340 <heathsmail0@gmail.com>
2023-02-16 12:00:28 -0700

    loading distance data from csv

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import csv
 
 
 hashTable = hash.ChainingHashTable(40)
+distancesList = []
 
 def main():
     #algo.tsp(dataSet) #to run algo
     loadPackageData("WGUPS Package File.csv")
+    loadDistanceData("WGUPS Distance Table.csv")
 
     return
 
 def loadPackageData(fileName):
     with open(fileName) as packages:
         packageData = csv.reader(packages, delimiter=',')
-        next(packageData) # skip header
+        next(packageData)
         for package in packageData:
             id = int(package[0])
             address = package[1]
@@ -28,8 +30,16 @@ def loadPackageData(fileName):
            
             pack = p.Package(id, address, city, state, zip, deadline, mass, note)
  
-            # insert it into the hash table
             hashTable.insert(id, p)
+
+def loadDistanceData(fileName):
+    with open(fileName) as distances:
+        distanceData = csv.reader(distances, delimiter=',')
+        next(distanceData)
+        for distance in distanceData:
+            d = [distance]
+
+            distancesList.append(d)
 
 
 if (__name__ == "__main__"):
