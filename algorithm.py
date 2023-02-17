b8346240052fdcb641241455b93edc2168806ab3
gheath340 <heathsmail0@gmail.com>
2023-02-17 11:33:20 -0700

    changed algorith

--- algorithm.py
+++ algorithm.py
@@ -1,161 +1,124 @@
-import unionFind as uf
-
-def tsp(data):
-    # build a graph
-    G = build_graph(data)
-    print("Graph: ", G)
-
-    # build a minimum spanning tree
-    MSTree = minimum_spanning_tree(G)
-    print("MSTree: ", MSTree)
-
-    # find odd vertexes
-    odd_vertexes = find_odd_vertexes(MSTree)
-    print("Odd vertexes in MSTree: ", odd_vertexes)
-
-    # add minimum weight matching edges to MST
-    minimum_weight_matching(MSTree, G, odd_vertexes)
-    print("Minimum weight matching: ", MSTree)
-
-    # find an eulerian tour
-    eulerian_tour = find_eulerian_tour(MSTree, G)
-
-    print("Eulerian tour: ", eulerian_tour)
-
-    current = eulerian_tour[0]
-    path = [current]
-    visited = [False] * len(eulerian_tour)
-    visited[eulerian_tour[0]] = True
-    length = 0
-
-    for v in eulerian_tour:
-        if not visited[v]:
-            path.append(v)
-            visited[v] = True
-
-            length += G[current][v]
-            current = v
-
-    length +=G[current][eulerian_tour[0]]
-    path.append(eulerian_tour[0])
-
-    print("Result path: ", path)
-    print("Result length of the path: ", length)
-
-    return length, path
+import graph
 
 
-def get_length(x1, y1, x2, y2):
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1.0 / 2.0)
-
-
-def build_graph(data):
-    graph = {}
-    for this in range(len(data)):
-        for another_point in range(len(data)):
-            if this != another_point:
-                if this not in graph:
-                    graph[this] = {}
-
-                graph[this][another_point] = get_length(data[this][0], data[this][1], data[another_point][0],
-                                                        data[another_point][1])
-
-    return graph
-
-
-def minimum_spanning_tree(G):
-    tree = []
-    subtrees = uf.UnionFind()
-    for W, u, v in sorted((G[u][v], u, v) for u in G for v in G[u]):
-        if subtrees[u] != subtrees[v]:
-            tree.append((u, v, W))
-            subtrees.union(u, v)
-
-    return tree
-
-
-def find_odd_vertexes(MST):
-    tmp_g = {}
-    vertexes = []
-    for edge in MST:
-        if edge[0] not in tmp_g:
-            tmp_g[edge[0]] = 0
-
-        if edge[1] not in tmp_g:
-            tmp_g[edge[1]] = 0
-
-        tmp_g[edge[0]] += 1
-        tmp_g[edge[1]] += 1
-
-    for vertex in tmp_g:
-        if tmp_g[vertex] % 2 == 1:
-            vertexes.append(vertex)
-
-    return vertexes
-
-
-def minimum_weight_matching(MST, G, odd_vert):
-    import random
-    random.shuffle(odd_vert)
-
-    while odd_vert:
-        v = odd_vert.pop()
-        length = float("inf")
-        u = 1
-        closest = 0
-        for u in odd_vert:
-            if v != u and G[v][u] < length:
-                length = G[v][u]
-                closest = u
-
-        MST.append((v, closest, length))
-        odd_vert.remove(closest)
-
-
-def find_eulerian_tour(MatchedMSTree, G):
-    # find neigbours
-    neighbours = {}
-    for edge in MatchedMSTree:
-        if edge[0] not in neighbours:
-            neighbours[edge[0]] = []
-
-        if edge[1] not in neighbours:
-            neighbours[edge[1]] = []
-
-        neighbours[edge[0]].append(edge[1])
-        neighbours[edge[1]].append(edge[0])
-
-    # print("Neighbours: ", neighbours)
-
-    # finds the hamiltonian circuit
-    start_vertex = MatchedMSTree[0][0]
-    EP = [neighbours[start_vertex][0]]
-
-    while len(MatchedMSTree) > 0:
-        for i, v in enumerate(EP):
-            if len(neighbours[v]) > 0:
-                break
-
-        while len(neighbours[v]) > 0:
-            w = neighbours[v][0]
-
-            remove_edge_from_matchedMST(MatchedMSTree, v, w)
-
-            del neighbours[v][(neighbours[v].index(w))]
-            del neighbours[w][(neighbours[w].index(v))]
-
-            i += 1
-            EP.insert(i, w)
-
-            v = w
-
-    return EP
-
-
-def remove_edge_from_matchedMST(MatchedMST, v1, v2):
-
-    for i, item in enumerate(MatchedMST):
-        if (item[0] == v2 and item[1] == v1) or (item[0] == v1 and item[1] == v2):
-            del MatchedMST[i]
-
-    return MatchedMST
+def dijkstra_shortest_path(g, start_vertex):
+    # Put all vertices in an unvisited queue.
+    unvisited_queue = []
+ 
+    for current_vertex in g.adjacency_list:
+        unvisited_queue.append(current_vertex) 
+            # unvisited_queue = [vertex_1, vertex_2, ...]
+ 
+    # Start_vertex has a distance of 0 from itself
+    start_vertex.distance = 0
+ 
+    # One vertex is removed with each iteration; repeat until the list is
+    # empty.
+    while len(unvisited_queue) > 0:
+        
+        # Visit vertex with minimum distance from start_vertex
+        smallest_index = 0
+        for i in range(1, len(unvisited_queue)):
+            #print(unvisited_queue[i].label, unvisited_queue[i].distance, unvisited_queue[i].pred_vertex)
+            if unvisited_queue[i].distance < unvisited_queue[smallest_index].distance:
+                smallest_index = i
+        current_vertex = unvisited_queue.pop(smallest_index)
+        #print("From Start Vetex to current_vertex.label: " + current_vertex.label +" distance: " + str(current_vertex.distance))
+ 
+        # Check potential path lengths from the current vertex to all neighbors.
+        for adj_vertex in g.adjacency_list[current_vertex]: # values from  dictionary
+                # if current_vertex = vertex_1 => adj_vertex in [vertex_2, vertex_3], if vertex_2 => adj_vertex in [vertex_6], ...
+            edge_weight = g.edge_weights[(current_vertex, adj_vertex)] # values from dictionary
+                # edge_weight = 484 then 626 then 1306, ...}
+            alternative_path_distance = current_vertex.distance + edge_weight
+                        
+            # If shorter path from start_vertex to adj_vertex is found, update adj_vertex's distance and predecessor
+            if alternative_path_distance < adj_vertex.distance:
+                adj_vertex.distance = alternative_path_distance
+                adj_vertex.pred_vertex = current_vertex
+                
+def get_shortest_path(start_vertex, end_vertex):
+    # Start from end_vertex and build the path backwards.
+    path = ""
+    current_vertex = end_vertex
+    while current_vertex is not start_vertex:
+        path = " -> " + str(current_vertex.label) + path
+        current_vertex = current_vertex.pred_vertex
+    path = start_vertex.label + path
+    return path
+ 
+def get_shortest_path_city(start_vertex, end_vertex):
+    # Start from end_vertex and build the path backwards.
+    path = ""
+    current_vertex = end_vertex
+    while current_vertex is not start_vertex:
+        myMovie = myHash.search(int(current_vertex.label))
+        path = " -> " + myMovie.city + path
+        current_vertex = current_vertex.pred_vertex
+    path = "Salt Lake City " + path
+    return path
+ 
+# Dijkstra shortest path main 
+# Program to find shortest paths from vertex A.
+g = graph.Graph()
+ 
+# add Vertices
+vertex_1 = graph.Vertex("1") # 1, "CITIZEN KANE", 1941, 25.00, Salt Lake City, Utah
+g.add_vertex(vertex_1)
+vertex_2 = graph.Vertex("2") # 2, "CASABLANCA", 1942, 25.00, Helena, Montana
+g.add_vertex(vertex_2)
+vertex_3 = graph.Vertex("3") # 3, "THE GODFATHER", 1972, 10.00, Santa Fe, New Mexico
+g.add_vertex(vertex_3) 
+vertex_4 = graph.Vertex("4") # 4, "GONE WITH THE WIND", 1939, 10.00, Austin, Texas
+g.add_vertex(vertex_4) 
+vertex_5 = graph.Vertex("5") # 5, "LAWRENCE OF ARABIA", 1962, 10.00, Lincoln, Nebraska
+g.add_vertex(vertex_5) 
+vertex_6 = graph.Vertex("6") # 6, "THE WIZARD OF OZ", 1939, 10.00, Madison, Wisconsin
+g.add_vertex(vertex_6) 
+vertex_7 = graph.Vertex("7") # 7, "THE GRADUATE", 1967, 5.00, New York, New York
+g.add_vertex(vertex_7) 
+vertex_8 = graph.Vertex("8") # 8, "ON THE WATERFRONT", 1954, 5.00, Columbus, Ohio
+g.add_vertex(vertex_8) 
+vertex_9 = graph.Vertex("9") # 9, "SCHINDLER'S LIST", 1993, 5.00, Raleigh, North Carolina
+g.add_vertex(vertex_9) 
+vertex_10 = graph.Vertex("10") # 10, "SINGIN' IN THE RAIN", 1952, 5.00, Orlando, Florida
+g.add_vertex(vertex_10) 
+vertex_11 = graph.Vertex("11") # 11, "STAR WARS", 1977, 1.00, Montgomery, Alabama
+g.add_vertex(vertex_11) 
+ 
+# add Edges 
+g.add_undirected_edge(vertex_1, vertex_2, 484) # 484 miles
+g.add_undirected_edge(vertex_1, vertex_3, 626)
+g.add_undirected_edge(vertex_2, vertex_6, 1306)
+g.add_undirected_edge(vertex_3, vertex_5, 774)
+g.add_undirected_edge(vertex_3, vertex_4, 687)
+g.add_undirected_edge(vertex_4, vertex_11, 797)
+g.add_undirected_edge(vertex_5, vertex_6, 482)
+g.add_undirected_edge(vertex_6, vertex_7, 936)
+g.add_undirected_edge(vertex_7, vertex_8, 535)
+g.add_undirected_edge(vertex_7, vertex_9, 504)
+g.add_undirected_edge(vertex_9, vertex_10, 594)
+g.add_undirected_edge(vertex_11, vertex_5, 970)
+g.add_undirected_edge(vertex_11, vertex_8, 664)
+g.add_undirected_edge(vertex_11, vertex_9, 567)
+g.add_undirected_edge(vertex_11, vertex_10, 453)
+ 
+# Run Dijkstra's algorithm first.
+dijkstra_shortest_path(g, vertex_1)
+ 
+# Get the vertices by the label for convenience; display shortest path for each vertex
+# from vertex_1.    
+print("\nDijkstra shortest path:")
+for v in g.adjacency_list:
+    if v.pred_vertex is None and v is not vertex_1:
+        print("1 to %s ==> no path exists" % v.label)
+    else:
+        print("1 to %s ==> %s (total distance: %g)" % (v.label, get_shortest_path(vertex_1, v), v.distance))
+ 
+print("\nDijkstra shortest path with Cities:")
+for v in g.adjacency_list:
+    myMovie = myHash.search(int(v.label))
+    if v.pred_vertex is None and v is not vertex_1:
+        print("Salt Lake City to %s ==> no path exists" % myMovie.city)
+    else:
+        print("Salt Lake City to %s ==> %s (total distance: %g)" % (myMovie.city, get_shortest_path_city(vertex_1, v), v.distance))
