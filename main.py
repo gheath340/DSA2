5be0c9c8211e03fa1b19bcd29d06c7db375a78cf
gheath340 <heathsmail0@gmail.com>
2023-02-17 10:20:32 -0700

    loaded trucks

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import package as p
 import csv
 
 
-hashTable = hash.ChainingHashTable(40)
-distancesList = []
-addressesList = []
+PACKAGES = hash.ChainingHashTable(40)
+DISTANCELIST = []
+ADDRESSLIST = []
 
 def main():
     #algo.tsp(dataSet) #to run algo
     loadPackageData("packageFile.csv")
     loadDistanceData("distanceFile.csv")
-    #loadAddressData("addressFile.csv")
-    for p in addressesList:
-        print(str(p) + "\n")
+    truck1 = [1, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40, 2, 4, 5]
+    truck2 = [3, 6, 18, 25, 36, 38, 7, 8, 10, 11, 12, 17, 21, 22, 23, 24]
+    truck3 = [9, 28, 32, 26, 27, 33, 35, 39]
 
 
 def loadPackageData(fileName):
@@ -33,29 +33,25 @@ def loadPackageData(fileName):
            
             pack = p.Package(id, address, city, state, zip, deadline, mass, note)
  
-            hashTable.insert(id, p)
+            PACKAGES.insert(id, p)
 
 
 def loadDistanceData(fileName):
     with open(fileName) as distances:
         distanceData = csv.reader(distances, delimiter=',')
-        next(distanceData)
         for distance in distanceData:
             a = distance[0]
             distance.pop(0)
             d = [distance]
 
-            addressesList.append(a)
-            distancesList.append(d)
+            ADDRESSLIST.append(a)
+            DISTANCELIST.append(d)
 
-
-def loadAddressData(fileName):
-    with open(fileName) as addresses:
-        addressData = csv.reader(addresses, delimiter=',')
-        next(addressData)
-        for address in addressData:
-            addressesList.append(address)
-
+#find out how to split the packages into 2 groups 1 for each truck
+    #use algo until 16 packages are loaded onto truck 1
+    #do the same thing for truck 2
+#use algo to load in best order
+#use algo to deliver
 
 if (__name__ == "__main__"):
     main()
