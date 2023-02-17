5b5c41314cec6e41c3cc190fd52a87343bf6167c
gheath340 <heathsmail0@gmail.com>
2023-02-17 11:50:40 -0700

    creating graphs for trucks

--- algorithm.py
+++ algorithm.py
@@ -1,124 +1,123 @@
-import graph
+# import graph
 
 
-def dijkstra_shortest_path(g, start_vertex):
-    # Put all vertices in an unvisited queue.
-    unvisited_queue = []
+# def dijkstra_shortest_path(g, start_vertex):
+#     # Put all vertices in an unvisited queue.
+#     unvisited_queue = []
  
-    for current_vertex in g.adjacency_list:
-        unvisited_queue.append(current_vertex) 
-            # unvisited_queue = [vertex_1, vertex_2, ...]
+#     for current_vertex in g.adjacency_list:
+#         unvisited_queue.append(current_vertex) 
+#             # unvisited_queue = [vertex_1, vertex_2, ...]
  
-    # Start_vertex has a distance of 0 from itself
-    start_vertex.distance = 0
+#     # Start_vertex has a distance of 0 from itself
+#     start_vertex.distance = 0
  
-    # One vertex is removed with each iteration; repeat until the list is
-    # empty.
-    while len(unvisited_queue) > 0:
+#     # One vertex is removed with each iteration; repeat until the list is
+#     # empty.
+#     while len(unvisited_queue) > 0:
         
-        # Visit vertex with minimum distance from start_vertex
-        smallest_index = 0
-        for i in range(1, len(unvisited_queue)):
-            #print(unvisited_queue[i].label, unvisited_queue[i].distance, unvisited_queue[i].pred_vertex)
-            if unvisited_queue[i].distance < unvisited_queue[smallest_index].distance:
-                smallest_index = i
-        current_vertex = unvisited_queue.pop(smallest_index)
-        #print("From Start Vetex to current_vertex.label: " + current_vertex.label +" distance: " + str(current_vertex.distance))
+#         # Visit vertex with minimum distance from start_vertex
+#         smallest_index = 0
+#         for i in range(1, len(unvisited_queue)):
+#             #print(unvisited_queue[i].label, unvisited_queue[i].distance, unvisited_queue[i].pred_vertex)
+#             if unvisited_queue[i].distance < unvisited_queue[smallest_index].distance:
+#                 smallest_index = i
+#         current_vertex = unvisited_queue.pop(smallest_index)
+#         #print("From Start Vetex to current_vertex.label: " + current_vertex.label +" distance: " + str(current_vertex.distance))
  
-        # Check potential path lengths from the current vertex to all neighbors.
-        for adj_vertex in g.adjacency_list[current_vertex]: # values from  dictionary
-                # if current_vertex = vertex_1 => adj_vertex in [vertex_2, vertex_3], if vertex_2 => adj_vertex in [vertex_6], ...
-            edge_weight = g.edge_weights[(current_vertex, adj_vertex)] # values from dictionary
-                # edge_weight = 484 then 626 then 1306, ...}
-            alternative_path_distance = current_vertex.distance + edge_weight
+#         # Check potential path lengths from the current vertex to all neighbors.
+#         for adj_vertex in g.adjacency_list[current_vertex]: # values from  dictionary
+#                 # if current_vertex = vertex_1 => adj_vertex in [vertex_2, vertex_3], if vertex_2 => adj_vertex in [vertex_6], ...
+#             edge_weight = g.edge_weights[(current_vertex, adj_vertex)] # values from dictionary
+#                 # edge_weight = 484 then 626 then 1306, ...}
+#             alternative_path_distance = current_vertex.distance + edge_weight
                         
-            # If shorter path from start_vertex to adj_vertex is found, update adj_vertex's distance and predecessor
-            if alternative_path_distance < adj_vertex.distance:
-                adj_vertex.distance = alternative_path_distance
-                adj_vertex.pred_vertex = current_vertex
+#             # If shorter path from start_vertex to adj_vertex is found, update adj_vertex's distance and predecessor
+#             if alternative_path_distance < adj_vertex.distance:
+#                 adj_vertex.distance = alternative_path_distance
+#                 adj_vertex.pred_vertex = current_vertex
                 
-def get_shortest_path(start_vertex, end_vertex):
-    # Start from end_vertex and build the path backwards.
-    path = ""
-    current_vertex = end_vertex
-    while current_vertex is not start_vertex:
-        path = " -> " + str(current_vertex.label) + path
-        current_vertex = current_vertex.pred_vertex
-    path = start_vertex.label + path
-    return path
+# def get_shortest_path(start_vertex, end_vertex):
+#     # Start from end_vertex and build the path backwards.
+#     path = ""
+#     current_vertex = end_vertex
+#     while current_vertex is not start_vertex:
+#         path = " -> " + str(current_vertex.label) + path
+#         current_vertex = current_vertex.pred_vertex
+#     path = start_vertex.label + path
+#     return path
  
-def get_shortest_path_city(start_vertex, end_vertex):
-    # Start from end_vertex and build the path backwards.
-    path = ""
-    current_vertex = end_vertex
-    while current_vertex is not start_vertex:
-        myMovie = myHash.search(int(current_vertex.label))
-        path = " -> " + myMovie.city + path
-        current_vertex = current_vertex.pred_vertex
-    path = "Salt Lake City " + path
-    return path
+# def get_shortest_path_city(start_vertex, end_vertex):
+#     # Start from end_vertex and build the path backwards.
+#     path = ""
+#     current_vertex = end_vertex
+#     while current_vertex is not start_vertex:
+#         myMovie = myHash.search(int(current_vertex.label))
+#         path = " -> " + myMovie.city + path
+#         current_vertex = current_vertex.pred_vertex
+#     path = "Salt Lake City " + path
+#     return path
  
-# Dijkstra shortest path main 
-# Program to find shortest paths from vertex A.
-g = graph.Graph()
+# #create a graph for each truck
+# g = graph.Graph()
  
-# add Vertices
-vertex_1 = graph.Vertex("1") # 1, "CITIZEN KANE", 1941, 25.00, Salt Lake City, Utah
-g.add_vertex(vertex_1)
-vertex_2 = graph.Vertex("2") # 2, "CASABLANCA", 1942, 25.00, Helena, Montana
-g.add_vertex(vertex_2)
-vertex_3 = graph.Vertex("3") # 3, "THE GODFATHER", 1972, 10.00, Santa Fe, New Mexico
-g.add_vertex(vertex_3) 
-vertex_4 = graph.Vertex("4") # 4, "GONE WITH THE WIND", 1939, 10.00, Austin, Texas
-g.add_vertex(vertex_4) 
-vertex_5 = graph.Vertex("5") # 5, "LAWRENCE OF ARABIA", 1962, 10.00, Lincoln, Nebraska
-g.add_vertex(vertex_5) 
-vertex_6 = graph.Vertex("6") # 6, "THE WIZARD OF OZ", 1939, 10.00, Madison, Wisconsin
-g.add_vertex(vertex_6) 
-vertex_7 = graph.Vertex("7") # 7, "THE GRADUATE", 1967, 5.00, New York, New York
-g.add_vertex(vertex_7) 
-vertex_8 = graph.Vertex("8") # 8, "ON THE WATERFRONT", 1954, 5.00, Columbus, Ohio
-g.add_vertex(vertex_8) 
-vertex_9 = graph.Vertex("9") # 9, "SCHINDLER'S LIST", 1993, 5.00, Raleigh, North Carolina
-g.add_vertex(vertex_9) 
-vertex_10 = graph.Vertex("10") # 10, "SINGIN' IN THE RAIN", 1952, 5.00, Orlando, Florida
-g.add_vertex(vertex_10) 
-vertex_11 = graph.Vertex("11") # 11, "STAR WARS", 1977, 1.00, Montgomery, Alabama
-g.add_vertex(vertex_11) 
+# #generate vertexes for each truck
+# vertex_1 = graph.Vertex("1") # 1, "CITIZEN KANE", 1941, 25.00, Salt Lake City, Utah
+# g.add_vertex(vertex_1)
+# vertex_2 = graph.Vertex("2") # 2, "CASABLANCA", 1942, 25.00, Helena, Montana
+# g.add_vertex(vertex_2)
+# vertex_3 = graph.Vertex("3") # 3, "THE GODFATHER", 1972, 10.00, Santa Fe, New Mexico
+# g.add_vertex(vertex_3) 
+# vertex_4 = graph.Vertex("4") # 4, "GONE WITH THE WIND", 1939, 10.00, Austin, Texas
+# g.add_vertex(vertex_4) 
+# vertex_5 = graph.Vertex("5") # 5, "LAWRENCE OF ARABIA", 1962, 10.00, Lincoln, Nebraska
+# g.add_vertex(vertex_5) 
+# vertex_6 = graph.Vertex("6") # 6, "THE WIZARD OF OZ", 1939, 10.00, Madison, Wisconsin
+# g.add_vertex(vertex_6) 
+# vertex_7 = graph.Vertex("7") # 7, "THE GRADUATE", 1967, 5.00, New York, New York
+# g.add_vertex(vertex_7) 
+# vertex_8 = graph.Vertex("8") # 8, "ON THE WATERFRONT", 1954, 5.00, Columbus, Ohio
+# g.add_vertex(vertex_8) 
+# vertex_9 = graph.Vertex("9") # 9, "SCHINDLER'S LIST", 1993, 5.00, Raleigh, North Carolina
+# g.add_vertex(vertex_9) 
+# vertex_10 = graph.Vertex("10") # 10, "SINGIN' IN THE RAIN", 1952, 5.00, Orlando, Florida
+# g.add_vertex(vertex_10) 
+# vertex_11 = graph.Vertex("11") # 11, "STAR WARS", 1977, 1.00, Montgomery, Alabama
+# g.add_vertex(vertex_11) 
  
-# add Edges 
-g.add_undirected_edge(vertex_1, vertex_2, 484) # 484 miles
-g.add_undirected_edge(vertex_1, vertex_3, 626)
-g.add_undirected_edge(vertex_2, vertex_6, 1306)
-g.add_undirected_edge(vertex_3, vertex_5, 774)
-g.add_undirected_edge(vertex_3, vertex_4, 687)
-g.add_undirected_edge(vertex_4, vertex_11, 797)
-g.add_undirected_edge(vertex_5, vertex_6, 482)
-g.add_undirected_edge(vertex_6, vertex_7, 936)
-g.add_undirected_edge(vertex_7, vertex_8, 535)
-g.add_undirected_edge(vertex_7, vertex_9, 504)
-g.add_undirected_edge(vertex_9, vertex_10, 594)
-g.add_undirected_edge(vertex_11, vertex_5, 970)
-g.add_undirected_edge(vertex_11, vertex_8, 664)
-g.add_undirected_edge(vertex_11, vertex_9, 567)
-g.add_undirected_edge(vertex_11, vertex_10, 453)
+# #generate edges for each truck
+# g.add_undirected_edge(vertex_1, vertex_2, 484) # 484 miles
+# g.add_undirected_edge(vertex_1, vertex_3, 626)
+# g.add_undirected_edge(vertex_2, vertex_6, 1306)
+# g.add_undirected_edge(vertex_3, vertex_5, 774)
+# g.add_undirected_edge(vertex_3, vertex_4, 687)
+# g.add_undirected_edge(vertex_4, vertex_11, 797)
+# g.add_undirected_edge(vertex_5, vertex_6, 482)
+# g.add_undirected_edge(vertex_6, vertex_7, 936)
+# g.add_undirected_edge(vertex_7, vertex_8, 535)
+# g.add_undirected_edge(vertex_7, vertex_9, 504)
+# g.add_undirected_edge(vertex_9, vertex_10, 594)
+# g.add_undirected_edge(vertex_11, vertex_5, 970)
+# g.add_undirected_edge(vertex_11, vertex_8, 664)
+# g.add_undirected_edge(vertex_11, vertex_9, 567)
+# g.add_undirected_edge(vertex_11, vertex_10, 453)
  
-# Run Dijkstra's algorithm first.
-dijkstra_shortest_path(g, vertex_1)
+# # Run Dijkstra's algorithm first.
+# dijkstra_shortest_path(g, vertex_1)
  
-# Get the vertices by the label for convenience; display shortest path for each vertex
-# from vertex_1.    
-print("\nDijkstra shortest path:")
-for v in g.adjacency_list:
-    if v.pred_vertex is None and v is not vertex_1:
-        print("1 to %s ==> no path exists" % v.label)
-    else:
-        print("1 to %s ==> %s (total distance: %g)" % (v.label, get_shortest_path(vertex_1, v), v.distance))
+# # Get the vertices by the label for convenience; display shortest path for each vertex
+# # from vertex_1.    
+# print("\nDijkstra shortest path:")
+# for v in g.adjacency_list:
+#     if v.pred_vertex is None and v is not vertex_1:
+#         print("1 to %s ==> no path exists" % v.label)
+#     else:
+#         print("1 to %s ==> %s (total distance: %g)" % (v.label, get_shortest_path(vertex_1, v), v.distance))
  
-print("\nDijkstra shortest path with Cities:")
-for v in g.adjacency_list:
-    myMovie = myHash.search(int(v.label))
-    if v.pred_vertex is None and v is not vertex_1:
-        print("Salt Lake City to %s ==> no path exists" % myMovie.city)
-    else:
-        print("Salt Lake City to %s ==> %s (total distance: %g)" % (myMovie.city, get_shortest_path_city(vertex_1, v), v.distance))
+# print("\nDijkstra shortest path with Cities:")
+# for v in g.adjacency_list:
+#     myMovie = myHash.search(int(v.label))
+#     if v.pred_vertex is None and v is not vertex_1:
+#         print("Salt Lake City to %s ==> no path exists" % myMovie.city)
+#     else:
+#         print("Salt Lake City to %s ==> %s (total distance: %g)" % (myMovie.city, get_shortest_path_city(vertex_1, v), v.distance))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import algorithm as algo
 import hashTable as hash
 import package as p
+import graph
 import csv
 
 
@@ -9,13 +10,30 @@ DISTANCELIST = []
 ADDRESSLIST = []
 
 def main():
-    #algo.tsp(dataSet) #to run algo
     loadPackageData("packageFile.csv")
     loadDistanceData("goodDistanceTable.csv")
     truck1 = [1, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40, 2, 4, 5]
     truck2 = [3, 6, 18, 25, 36, 38, 7, 8, 10, 11, 12, 17, 21, 22, 23, 24]
     truck3 = [9, 28, 32, 26, 27, 33, 35, 39]
-
+    #MAPPING ALL DISTANCES OF ADDRESSES TO EACHOTHER
+    #create graph for each truck
+    t1Graph = graph.Graph()
+    t2Graph = graph.Graph()
+    t3Graph = graph.Graph()
+    #generate vertecies for each truck
+    for i in range(len(truck1)):
+        vertex = graph.Vertex(str(truck1[i]))
+        t1Graph.add_vertex(vertex)
+    for i in range(len(truck2)):
+        vertex = graph.Vertex(str(truck2[i]))
+        t2Graph.add_vertex(vertex)
+    for i in range(len(truck3)):
+        vertex = graph.Vertex(str(truck3[i]))
+        t3Graph.add_vertex(vertex)
+    #generate edges for each truck(from each vertex to every other vertex) length^2
+    for i in range(len(truck1)):
+        for j in range(len(truck1)):
+            t1Graph.add_undirected_edge(truck1[i], truck1[j], )
 
 
 def loadPackageData(fileName):
@@ -47,8 +65,6 @@ def loadDistanceData(fileName):
 
             ADDRESSLIST.append(a)
             DISTANCELIST.append(d)
-
-#create the graph w/ location as key and list of distances as value list
 
 
 if (__name__ == "__main__"):
