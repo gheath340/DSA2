7c9e1ad0536cb6832501a6a42b3b5bfb53001455
gheath340 <heathsmail0@gmail.com>
2023-02-18 13:31:27 -0700

    dijkstras works

--- algorithm.py
+++ algorithm.py
@@ -1,41 +1,41 @@
-# import graph
+import graph
 
 
-# def dijkstra_shortest_path(g, start_vertex):
-#     # Put all vertices in an unvisited queue.
-#     unvisited_queue = []
+def dijkstra_shortest_path(g, start_vertex):
+    # Put all vertices in an unvisited queue.
+    unvisited_queue = []
  
-#     for current_vertex in g.adjacency_list:
-#         unvisited_queue.append(current_vertex) 
-#             # unvisited_queue = [vertex_1, vertex_2, ...]
+    for current_vertex in g.adjacency_list:
+        unvisited_queue.append(current_vertex) 
+            # unvisited_queue = [vertex_1, vertex_2, ...]
  
-#     # Start_vertex has a distance of 0 from itself
-#     start_vertex.distance = 0
+    # Start_vertex has a distance of 0 from itself
+    start_vertex.distance = 0
  
-#     # One vertex is removed with each iteration; repeat until the list is
-#     # empty.
-#     while len(unvisited_queue) > 0:
+    # One vertex is removed with each iteration; repeat until the list is
+    # empty.
+    while len(unvisited_queue) > 0:
         
-#         # Visit vertex with minimum distance from start_vertex
-#         smallest_index = 0
-#         for i in range(1, len(unvisited_queue)):
-#             #print(unvisited_queue[i].label, unvisited_queue[i].distance, unvisited_queue[i].pred_vertex)
-#             if unvisited_queue[i].distance < unvisited_queue[smallest_index].distance:
-#                 smallest_index = i
-#         current_vertex = unvisited_queue.pop(smallest_index)
-#         #print("From Start Vetex to current_vertex.label: " + current_vertex.label +" distance: " + str(current_vertex.distance))
+        # Visit vertex with minimum distance from start_vertex
+        smallest_index = 0
+        for i in range(1, len(unvisited_queue)):
+            #print(unvisited_queue[i].label, unvisited_queue[i].distance, unvisited_queue[i].pred_vertex)
+            if unvisited_queue[i].distance < unvisited_queue[smallest_index].distance:
+                smallest_index = i
+        current_vertex = unvisited_queue.pop(smallest_index)
+        #print("From Start Vetex to current_vertex.label: " + current_vertex.label +" distance: " + str(current_vertex.distance))
  
-#         # Check potential path lengths from the current vertex to all neighbors.
-#         for adj_vertex in g.adjacency_list[current_vertex]: # values from  dictionary
-#                 # if current_vertex = vertex_1 => adj_vertex in [vertex_2, vertex_3], if vertex_2 => adj_vertex in [vertex_6], ...
-#             edge_weight = g.edge_weights[(current_vertex, adj_vertex)] # values from dictionary
-#                 # edge_weight = 484 then 626 then 1306, ...}
-#             alternative_path_distance = current_vertex.distance + edge_weight
+        # Check potential path lengths from the current vertex to all neighbors.
+        for adj_vertex in g.adjacency_list[current_vertex]: # values from  dictionary
+                # if current_vertex = vertex_1 => adj_vertex in [vertex_2, vertex_3], if vertex_2 => adj_vertex in [vertex_6], ...
+            edge_weight = g.edge_weights[(current_vertex, adj_vertex)] # values from dictionary
+                # edge_weight = 484 then 626 then 1306, ...}
+            alternative_path_distance = current_vertex.distance + float(edge_weight)
                         
-#             # If shorter path from start_vertex to adj_vertex is found, update adj_vertex's distance and predecessor
-#             if alternative_path_distance < adj_vertex.distance:
-#                 adj_vertex.distance = alternative_path_distance
-#                 adj_vertex.pred_vertex = current_vertex
+            # If shorter path from start_vertex to adj_vertex is found, update adj_vertex's distance and predecessor
+            if alternative_path_distance < adj_vertex.distance:
+                adj_vertex.distance = alternative_path_distance
+                adj_vertex.pred_vertex = current_vertex
                 
 # def get_shortest_path(start_vertex, end_vertex):
 #     # Start from end_vertex and build the path backwards.
@@ -106,7 +106,7 @@
 # dijkstra_shortest_path(g, vertex_1)
  
 # # Get the vertices by the label for convenience; display shortest path for each vertex
-# # from vertex_1.    
+# from vertex_1.    
 # print("\nDijkstra shortest path:")
 # for v in g.adjacency_list:
 #     if v.pred_vertex is None and v is not vertex_1:
--- main.py
+++ main.py
@@ -9,30 +9,80 @@ PACKAGES = hash.ChainingHashTable(40)
 DISTANCEDICT = {}
 ADDRESSLIST = []
 
+#package ids for each truck
+truck1 = [1, 2, 4, 5, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40]
+truck2 = [3, 6, 7, 8, 10, 11, 12, 17, 18, 21, 22, 23, 24, 25, 36, 38]
+truck3 = [9, 26, 27, 28, 32, 33, 35, 39]
+#create graph for each truck
+t1Graph = graph.Graph()
+t2Graph = graph.Graph()
+t3Graph = graph.Graph()
 
+#LEFT OFF
+    #can call dijkstras and it runs successfully
+    #need to figure out whats next
+    #how to run through vertecies in the best way?
 def main():
     loadPackageData("packageFile.csv")
     loadDistanceData("goodDistanceTable.csv")
-    truck1 = [1, 2, 4, 5, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40]
-    truck2 = [3, 6, 7, 8, 10, 11, 12, 17, 18, 21, 22, 23, 24, 25, 36, 38]
-    truck3 = [9, 26, 27, 28, 32, 33, 35, 39]
-    #FIGURING OUT HOW IM MAKING GRAPHS AND HOW IM CONNECTING ALL VERTEXES
-    #create graph for each truck
-    t1Graph = graph.Graph()
-    t2Graph = graph.Graph()
-    t3Graph = graph.Graph()
-    # for distance in DISTANCEDICT:
-    #     print(distance + str(DISTANCEDICT[distance]))
+    generateVertecies()
+    startVertexEdgesCreate()
+    generateEdges()
+    algo.dijkstra_shortest_path(t1Graph, t1Graph.vertexList[0])
+
+    print("\nDijkstra shortest path:")
+    for v in t1Graph.adjacency_list:
+        if v.pred_vertex is None and v is not t1Graph.vertexList[0]:
+            print("HUB to %s ==> no path exists" % v.label)
+        else:
+            print("HUB to %s ==> %s (total distance: %g)" % (v.label, get_shortest_path(t1Graph.vertexList[0], v), v.distance))
+    
+def get_shortest_path(start_vertex, end_vertex):
+    # Start from end_vertex and build the path backwards.
+    path = ""
+    current_vertex = end_vertex
+    while current_vertex is not start_vertex:
+        path = " -> " + str(current_vertex.label) + path
+        current_vertex = current_vertex.pred_vertex
+    path = str(start_vertex.label) + path
+    return path
+
+def generateVertecies():
     #generate vertecies for each truck
-    for i in range(len(truck1)):
-        vertex = graph.Vertex(truck1[i])
-        t1Graph.add_vertex(vertex)
-    for i in range(len(truck2)):
-        vertex = graph.Vertex(truck2[i])
-        t2Graph.add_vertex(vertex)
-    for i in range(len(truck3)):
-        vertex = graph.Vertex(truck3[i])
-        t3Graph.add_vertex(vertex)
+    v = graph.Vertex(0)
+    for g in [t1Graph, t2Graph, t3Graph]:
+        g.add_vertex(v)
+    for truck in [truck1, truck2, truck3]:
+        for i in range(len(truck)):
+            vertex = graph.Vertex(truck[i])
+            if truck == truck1:
+                t1Graph.add_vertex(vertex)
+            elif truck == truck2:
+                t2Graph.add_vertex(vertex)
+            else:
+                t3Graph.add_vertex(vertex)
+
+
+def startVertexEdgesCreate():
+    for truck in [truck1, truck2, truck3]:
+        for i in range(len(truck)):
+            package = PACKAGES.search(truck[i])
+            packageAddress = package.getAddress()
+            dList = DISTANCEDICT["\ufeffHUB"]
+            counter = 0
+            while ADDRESSLIST[counter] != packageAddress:
+                counter += 1
+            distance = dList[0][counter]
+            if truck == truck1:
+                t1Graph.add_undirected_edge(t1Graph.vertexList[0], t1Graph.vertexList[truck[i]], distance)
+            elif truck == truck2:
+                t2Graph.add_undirected_edge(t2Graph.vertexList[0], t2Graph.vertexList[truck[i]], distance)
+            else:
+                t3Graph.add_undirected_edge(t3Graph.vertexList[0], t3Graph.vertexList[truck[i]], distance)
+
+    return
+
+def generateEdges():
     #generate edges for each truck
     for truck in [truck1, truck2, truck3]:
         for i in range(len(truck)):
@@ -55,6 +105,7 @@ def main():
                     t3Graph.add_undirected_edge(t3Graph.vertexList[truck[i]], t3Graph.vertexList[truck[j]], distance)
 
 
+#load package data into items and hashTable
 def loadPackageData(fileName):
     with open(fileName) as packages:
         packageData = csv.reader(packages, delimiter=',')
@@ -73,7 +124,7 @@ def loadPackageData(fileName):
  
             PACKAGES.insert(id, pack)
 
-
+#load distance data into distance dictionary and addresslist
 def loadDistanceData(fileName):
     with open(fileName) as distances:
         distanceData = csv.reader(distances, delimiter=',')
